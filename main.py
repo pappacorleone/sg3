b3d0f2bd0f68aeb42793341f11c9fa55bf801a49
Kris Mondlane <kmondlane@gmail.com>
2022-12-28 22:05:58 +0100

    New Main.py + Menu.py Addition

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# In main.py
 import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -5,12 +6,15 @@ import user_database
 import checklist_database
 import task_habit_database
 import carryover
-import select_task_habit
-import end_of_weekday_prompt
+import select_tasks_habits
+import end_of_weekday
 import generate_daily_infographic
 import generate_weekly_infographic
 import multiple_choice_menu
 import logout
+
+# Dictionary to store the MultipleChoiceMenu object for each user
+menu_dict = {}
 
 def start(update, context):
   # Welcome message and instructions for using the chatbot
@@ -47,8 +51,18 @@ def login(update, context):
     update.message.reply_text("Invalid email or password.")
 
 def menu(update, context):
-  # Display multiple choice menu options
-  multiple_choice_menu.navigate(multiple_choice_menu.root)
+  # Get the user's chat ID
+  chat_id = update.message.chat_id
+  # Check if the user has a MultipleChoiceMenu object in the dictionary
+  if chat_id in menu_dict:
+    # Display the user's menu options
+    menu_dict[chat_id].display_options()
+  else:
+    # Create a new MultipleChoiceMenu object for the user and add it to the dictionary
+    menu = multiple_choice_menu.MultipleChoiceMenu()
+    menu_dict[chat_id] = menu
+    # Display the user's menu options
+    menu.display_options()
 
 def create_checklist(update, context):
   # Prompt user to enter the name and description of their checklist
@@ -63,8 +77,19 @@ def create_checklist(update, context):
   checklist_database.add_checklist(checklist)
   update.message.reply_text("Checklist '{}' has been created successfully.".format(name))
 
-
 def create_task_habit(update, context):
+  # Get the user's chat ID
+  chat_id = update.message.chat_id
+  # Check if the user has a MultipleChoiceMenu object in the dictionary
+  if chat_id in menu_dict:
+    # Display the user's menu options
+    menu_dict[chat_id].display_options()
+  else:
+    # Create a new MultipleChoiceMenu object for the user and add it to the dictionary
+    menu = multiple_choice_menu.MultipleChoiceMenu()
+    menu_dict[chat_id] = menu
+    # Display the user's menu options
+    menu.display_options()
   # Prompt user to enter the name, description, frequency, and priority of their task or habit
   update.message.reply_text("Please enter the name, description, frequency, and priority of your task or habit, separated by spaces. (Frequency should be 'daily' or 'weekly', priority should be a number from 1 to 5, with 1 being the highest priority.)")
   # Wait for user's response
@@ -80,56 +105,77 @@ def create_task_habit(update, context):
   elif frequency == 'weekly':
     task_habit_database.add_habit(name, description, frequency, priority)
   else:
-    update.message.reply_text("Invalid frequency. Please enter 'daily' or 'weekly'.")
+    task_habit_database.add_task(name, description, priority)
+  update.message.reply_text("Task or habit '{}' has been added successfully.".format(name))
 
-def carryover_tasks(update, context):
-  # Carry over previous week's uncompleted tasks and habits
-  checklist = checklist_database.get_current_checklist()
-  carryover.carryover(checklist)
-  update.message.reply_text("Previous week's uncompleted tasks and habits have been carried over.")
+def update_task_habit(update, context):
+  # Prompt user to enter the name and new status of the task or habit they want to update
+  update.message.reply_text("Please enter the name and new status of the task or habit you want to update, separated by a space. (Status should be 'completed' or 'incomplete'.)")
+  # Wait for user's response
+  name, status = context.bot.wait_for(
+      'message', check=lambda message: message.chat_id == update.message.chat_id)
+  name = name.strip()
+  status = status.strip().lower()
+  # Update task or habit in the database
+  task_habit_database.update_task_habit(name, status)
+  update.message.reply_text("Task or habit '{}' has been updated successfully.".format(name))
 
-def select_tasks_habits(update, context):
-  # Recommend tasks and habits based on past performance and user preferences
-  checklist = checklist_database.get_current_checklist()
-  past_performance = task_habit_database.get_past_performance()
-  user_preferences = task_habit_database.get_user_preferences()
-  recommended_tasks, recommended_habits = select_task_habit.select_task_habit(checklist, past_performance, user_preferences)
-  update.message.reply_text("Here are some tasks and habits you might want to consider for today:\n\n"
-                            "Tasks:\n" + '\n'.join([task.name for task in recommended_tasks]) + "\n\n"
-                            "Habits:\n" + '\n'.join([habit.name for habit in recommended_habits]))
+def delete_task_habit(update, context):
+  # Get the user's chat ID
+  chat_id = update.message.chat_id
+  # Check if the user has a MultipleChoiceMenu object in the dictionary
+  if chat_id in menu_dict:
+    # Display the user's menu options
+    menu_dict[chat_id].display_options()
+  else:
+    # Create a new MultipleChoiceMenu object for the user and add it to the dictionary
+    menu = multiple_choice_menu.MultipleChoiceMenu()
+    menu_dict[chat_id] = menu
+    # Display the user's menu options
+    menu.display_options()
+  # Prompt user to enter the name of the task or habit they want to delete
+  update.message.reply_text("Please enter the name of the task or habit you want to delete.")
+  # Wait for user's response
+  name = context.bot.wait_for(
+      'message', check=lambda message: message.chat_id == update.message.chat_id)
+  name = name.strip()
+  # Delete task or habit from the database
+  task_habit_database.delete_task_habit(name)
+  update.message.reply_text("Task or habit '{}' has been deleted successfully.".format(name))
+
+def carryover_checklist(update, context):
+  # Carry over previous week's uncompleted checklist items
+  carryover.carryover_checklist()
+  update.message.reply_text("Previous week's uncompleted checklist items have been carried over.")
+
+def select_task_habit_today(update, context):
+  # Ask user to select a task or habit for today
+  select_tasks_habits.select_task_habit_today()
+  update.message.reply_text("Please select a task or habit for today from the list provided.")
 
 def end_of_weekday(update, context):
-  # Predict likely tasks and habits that will be completed based on past performance
-  checklist = checklist_database.get_current_checklist()
-  past_performance = task_habit_database.get_past_performance()
-  likely_completed_tasks = end_of_weekday_prompt.end_of_weekday_prompt(checklist, past_performance)
-  update.message.reply_text("Based on your past performance, here are some tasks and habits you are likely to complete today:\n\n"
-                            + '\n'.join([task.name for task in likely_completed_tasks]) + "\n\n"
-                            "Would you like to review your progress?")
+  # Prompt user to update their list and review their progress
+  end_of_weekday.end_of_weekday_prompt()
+  update.message.reply_text("Please update your list and review your progress for the day.")
 
-def generate_daily_report(update, context):
-  # Generate personalized infographic summary of user's progress through their checklist
-  checklist = checklist_database.get_current_checklist()
-  infographic = generate_daily_infographic.generate_daily_infographic(checklist)
-  update.message.reply_text("Here is a summary of your progress today:\n\n" + infographic)
+def generate_daily_infographic(update, context):
+  # Generate and send personalized infographic summary of the user's progress through their checklist
+  generate_daily_infographic.generate_daily_infographic()
+  update.message.reply_text("Your personalized infographic summary for the day has been sent to your chat.")
 
-def generate_weekly_report(update, context):
-  # Generate more comprehensive personalized infographic summary of user's progress towards accomplishing their weekly checklist
-  checklist = checklist_database.get_current_checklist()
-  infographic = generate_weekly_infographic.generate_weekly_infographic(checklist)
-  update.message.reply_text("Here is a summary of your progress this week:\n\n" + infographic)
+def generate_weekly_infographic(update, context):
+  # Generate and send more comprehensive personalized infographic summary of the user's progress towards accomplishing their weekly checklist
+  generate_weekly_infographic.generate_weekly_infographic()
+  update.message.reply_text("Your personalized infographic summary for the week has been sent to your chat.")
 
 def logout(update, context):
-  # Logout user and clear their session
-  user_database.logout()
-  update.message.reply_text("You have been logged out successfully.")
+  # Logout user from their account
+  logout.logout()
+  update.message.reply_text("You have been logged out of your account.")
 
 def main():
-  # Initialize bot and updater
-  TOKEN = "YOUR_API_TOKEN"
-  bot = telegram.Bot(token=TOKEN)
-  updater = Updater(token=TOKEN, use_context=True)
-
+  # Create Telegram bot and get updates
+  updater = Updater(TOKEN, use_context=True)
   # Add command handlers
   updater.dispatcher.add_handler(CommandHandler('start', start))
   updater.dispatcher.add_handler(CommandHandler('create_account', create_account))
@@ -137,11 +183,14 @@ def main():
   updater.dispatcher.add_handler(CommandHandler('menu', menu))
   updater.dispatcher.add_handler(CommandHandler('create_checklist', create_checklist))
   updater.dispatcher.add_handler(CommandHandler('create_task_habit', create_task_habit))
-  updater.dispatcher.add_handler(CommandHandler('carryover_tasks', carryover_tasks))
+  updater.dispatcher.add_handler(CommandHandler('update_task_habit', update_task_habit))
+  updater.dispatcher.add_handler(CommandHandler('delete_task_habit', delete_task_habit))
+  updater.dispatcher.add_handler(CommandHandler('carryover_checklist', carryover_checklist))
+  updater.dispatcher.add_handler(CommandHandler('carryover_tasks', carryover))
   updater.dispatcher.add_handler(CommandHandler('select_tasks_habits', select_tasks_habits))
   updater.dispatcher.add_handler(CommandHandler('end_of_weekday', end_of_weekday))
-  updater.dispatcher.add_handler(CommandHandler('generate_daily_report', generate_daily_report))
-  updater.dispatcher.add_handler(CommandHandler('generate_weekly_report', generate_weekly_report))
+  updater.dispatcher.add_handler(CommandHandler('generate_daily_report', generate_daily_infographic))
+  updater.dispatcher.add_handler(CommandHandler('generate_weekly_report', generate_weekly_infographic))
   updater.dispatcher.add_handler(CommandHandler('logout', logout))
 
   # Start the bot
@@ -150,3 +199,4 @@ def main():
 
 if __name__ == '__main__':
   main()
+
--- multiple_choice_menu.py
+++ multiple_choice_menu.py
@@ -1,3 +1,7 @@
+
+from telegram import Update
+
+
 class MenuOption:
   def __init__(self, text, children=None, action=None):
     self.text = text
@@ -28,3 +32,13 @@ class MultipleChoiceMenu:
         print("Invalid input. Please enter a valid number.")
       except IndexError:
         print("Invalid choice. Please enter a valid number.")
+        
+  def display_menu(menu):
+  # Display the menu options
+    options = menu.display_options()
+  Update.message.reply_text(options)
+  # Wait for user's response
+  response = context.bot.wait_for_message(
+      check=lambda message: message.chat_id == update.message.chat_id)
+  # Handle the user's response
+  menu.navigate(response.text)
